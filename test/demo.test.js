d5cdb68e0cab1ecedbf404d64cf96786e358b2e9
Jorge Mosqueda <mosqueda.ing@icloud.com>
2022-07-03 13:21:22 -0500

    Añadiendo types para jest y primer test

--- test/demo.test.js
+++ test/demo.test.js
@@ -1,1 +1,10 @@
-test("Esta prueba no debe de fallar", () => {});
+describe("Primer test cons jest", () => {
+  test("Esta prueba no debe de fallar", () => {
+    // 1. Inicialización
+    const message = "Hola Mundo";
+    // 2. Estimulo
+    const message2 = message.trim();
+    // 3. Observar el comportamiento... esperado
+    expect(message).toBe(message2);
+  });
+});

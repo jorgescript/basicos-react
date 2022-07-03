398fd5eb733b6ce5142632e20b84cc1e6e47cccc
Jorge Mosqueda <mosqueda.ing@icloud.com>
2022-07-03 15:27:04 -0500

    Testing funciones asincronas, configuracion jest (usar fetch)

--- test/funciones.test.js
+++ test/funciones.test.js
@@ -1,16 +1,39 @@
 const getUser = () => ({ uid: "ABC123", username: "jorgescript" });
 const getUserActive = (name) => ({ uid: "ABC123", username: name });
+const returnArray = () => ["ABC", 123];
+const getPokemon = async (idPokemon) => {
+  try {
+    const url = `https://pokeapi.co/api/v2/pokemon/${idPokemon}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const { name } = data;
+    return name;
+  } catch (error) {
+    return "No existe el pokemon";
+  }
+};
 
 describe("Pruebas en funciones", () => {
   test("Prueba en la funcion getUser", () => {
     const testUser = { uid: "ABC123", username: "jorgescript" };
     const user = getUser();
-    expect(testUser).toEqual(user);
+    expect(user).toEqual(testUser);
   });
 
   test("Prueba en funcion getUserActive", () => {
     const testUser = { uid: "ABC123", username: "jorgescript" };
     const user = getUserActive("jorgescript");
-    expect(testUser).toEqual(user);
+    expect(user).toEqual(testUser);
+  });
+
+  test("Debe retornar un arreglo de un string y un numero", () => {
+    const [letters, numbers] = returnArray();
+    expect(letters).toEqual(expect.any(String));
+    expect(numbers).toEqual(expect.any(Number));
+  });
+
+  test("Prueba de la funcion getPokemon", async () => {
+    const bulbasaur = await getPokemon(1);
+    expect(bulbasaur).toBe("bulbasaur");
   });
 });
